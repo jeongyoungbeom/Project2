ce510f3e4c91284a3ab7acb501d0d60fb71f7465
jeong young beom <jybeomss1@gmail.com>
2022-04-30 22:12:25 +0900

    22-04-30

--- backend/routes/inquiry.js
+++ backend/routes/inquiry.js
@@ -11,71 +11,28 @@ router.use(cors({origin : 'http://localhost:3000', credentials : true, methods :
 
 
 // 문의하기
-router.route('/inquiry').post((req, res) => {
-    const memberIdx = req.body.memberIdx;
-    const respondent = req.body.respondent;
-    const title = req.body.title;
-    const content = req.body.content;
-    const type = req.body.type;
-    if (pool) {
-        inquiry(memberIdx, title, content, type, respondent, (err, result) => {
-            if (err) {
-                res.writeHead('200', { 'content-type': 'text/html; charset=utf8' });
-                res.write('<h2>메인데이터 출력 실패 </h2>');
-                res.write('<p>데이터가 안나옵니다.</p>')
-                res.end();
-            } else {
-                res.send(result);
-            }
-        })
-    }
-})
-
 router.post('/inquiry', async (req, res, next) => {
     try {
         const respondent = req.body.respondent;
         if (respondent === undefined) {
             Inquiry.create({
-
+                title: req.body.title,
+                content: req.body.content,
+                memberId: req.body.memberId
             })
         } else {
-
+            Inquiry.create({
+                title: req.body.title,
+                content: req.body.content,
+                respondent,
+                memberId: req.body.memberId
+            })
         }
     } catch (err) {
         console.log(err);
         next(err);
     }
-})
-//문의 하기
-const inquiry = function (memberIdx, title, content, type, respondent, callback) {
-    pool.getConnection((err, conn) => {
-        if (err) {
-            console.log(err);
-        } else {
-            if (respondent == undefined || respondent == 'undefined') {
-                conn.query('insert into inquiry(memberIdx, title, content, type) values (?,?,?,?)', [memberIdx, title, content, type], (err, result) => {
-                    conn.release();
-                    if (err) {
-                        callback(err, null);
-                        return;
-                    } else {
-                        callback(null, true);
-                    }
-                })
-            } else {
-                conn.query('insert into inquiry(memberIdx, title, content, type, respondent) values (?,?,?,?,?)', [memberIdx, title, content, type, respondent], (err, result) => {
-                    conn.release();
-                    if (err) {
-                        callback(err, null);
-                        return;
-                    } else {
-                        callback(null, true);
-                    }
-                })
-            }
-        }
-    });
-}
+});
 
 // 사용자 문의 내역
 router.route('/member/inquirylist').get((req, res) => {
